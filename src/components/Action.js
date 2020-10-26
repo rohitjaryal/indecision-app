bb51ebdd24b76353719330c98c81cd8a16c67da0
rohit jaryal <rohit.jaryal@live.com>
2020-10-26 22:00:17 +0700

    before react router

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
+const Action = (props) => (
+  <div>
+    <button
+      className="big-button"
+      onClick={props.handlePick}
+      disabled={!props.hasOptions}
+    >
+      What should I do?
+      </button>
+  </div>
+);
 
-const Action = (props) => {
-    return (
-      <div>
-        <button
-          onClick={props.handlePick}
-          disabled={!props.hasOptions}
-        >
-          What should I do?
-        </button>
-      </div>
-    );
-  };
-
-  export default Action;
+export default Action;
--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,59 +1,33 @@
 class VisibilityToggle extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleVisibility = this.handleVisibility.bind(this);
-        this.state = {
-            visibility: false
-        };
-    }
-
-
-    handleVisibility() {
-        this.setState((prevState) => {
-            console.log("prev state", prevState.visibility);
-            return { visibility: !prevState.visibility };
-        });
-
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Visibility Toggle</h1>
-                <button onClick={this.handleVisibility}>{this.state.visibility? <p>hide</p>:<p>show</p>}</button>
-                {this.state.visibility && <h3 >Details</h3>}
-            </div>
-
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    this.state = {
+      visibility: false
+    };
+  }
+  handleToggleVisibility() {
+    this.setState((prevState) => {
+      return {
+        visibility: !prevState.visibility
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.handleToggleVisibility}>
+          {this.state.visibility ? 'Hide details' : 'Show details'}
+        </button>
+        {this.state.visibility && (
+          <div>
+            <p>Hey. These are some details you can now see!</p>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
-
-
-// const heading= 'Visibility toggle';
-// const conditionalContent= 'This can be shown now!';
-
-// let isLabelHidden=true;
-
-// const renderApp=() => {
-//     const template=(
-//         <div>
-//             <h1>{heading}</h1>
-//             <button onClick={()=> {
-//                 console.log('clicked with value',isLabelHidden);
-//                 isLabelHidden=!isLabelHidden;
-//                 renderApp();
-//             }}>Show details</button>
-//             <h3 hidden={isLabelHidden}>{conditionalContent}</h3>
-//         </div>
-//     );
-
-//     ReactDOM.render(template,appRoot);
-// };
-
-// const appRoot=document.getElementById('app');
-
-
-
-// renderApp();
